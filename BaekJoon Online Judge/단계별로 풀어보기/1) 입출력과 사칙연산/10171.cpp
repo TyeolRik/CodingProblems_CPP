bd644c77d3cdd048d5c8053319f20a2a0c84290f
TyeolRik <kino6147@naver.com>
2020-08-31 17:49:16 +0000

    testing

---
+++ BaekJoon Online Judge/단계별로 풀어보기/1) 입출력과 사칙연산/10171.cpp
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/10171
+#include <iostream>
+
+int main() {
+    std::cout << "\\    /\\" << std::endl;
+    std::cout << " )  ( ')" << std::endl;
+    std::cout << "(  /  )" << std::endl;
+    std::cout << " \\(__)|" << std::endl;
+}
---
+++ BaekJoon Online Judge/단계별로 풀어보기/1) 입출력과 사칙연산/10172.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10172
+#include <iostream>
+
+int main() {
+    std::cout << "|\\_/|" << std::endl;
+    std::cout << "|q p|   /}" << std::endl;
+    std::cout << "( 0 )\"\"\"\\" << std::endl;
+    std::cout << "|\"^\"`    |" << std::endl;
+    std::cout << "||_/=\\\\__|" << std::endl;
+}
