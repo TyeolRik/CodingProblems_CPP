fcc2ecfb6952b783338fe987ad278b480dcc6087
TyeolRik <kino6147@naver.com>
2020-09-06 23:11:35 +0000

    Now Solving

--- BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1193.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1193.cpp
@@ -1,3 +1,25 @@
 // https://www.acmicpc.net/problem/1193
 #include <iostream>
 
+// 1 2 4 7 11 ...
+int main() {
+    int X;
+    std::cin >> X;
+    int now = 1;
+    int d = 1;
+    while(true) {
+        if(now >= X) {
+            break;
+        }
+        now = now + d;
+        d = d + 1;
+    }
+    std::cout << now << std::endl;
+    int denominator = 1;
+    int numerator = d - 1;
+    while(now -->= X) {
+        denominator++;
+        numerator--;
+    }
+    std::cout << numerator << "/" << denominator;
+}
