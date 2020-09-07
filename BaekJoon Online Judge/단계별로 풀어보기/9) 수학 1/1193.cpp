37be29a71be4649d1b8349855048a641a7f00e99
TyeolRik <kino6147@naver.com>
2020-09-07 21:07:11 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1193.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1193.cpp
@@ -1,25 +1,44 @@
 // https://www.acmicpc.net/problem/1193
 #include <iostream>
 
-// 1 2 4 7 11 ...
+// 1 2 6 7 15 16 28 29 45 46
 int main() {
     int X;
     std::cin >> X;
-    int now = 1;
+    
+    bool direction = true;      // TRUE: 좌하향, FALSE: 우상향
+    int temp = 1;
+    int mother = 1;
+    int son = 1;
+    int a = 1;
     int d = 1;
-    while(true) {
-        if(now >= X) {
-            break;
-        }
-        now = now + d;
+
+    while(X >= a) {
+        a = a + d;
         d = d + 1;
+        temp++;
+        direction = !direction;
     }
-    std::cout << now << std::endl;
-    int denominator = 1;
-    int numerator = d - 1;
-    while(now -->= X) {
-        denominator++;
-        numerator--;
+    a = a - d + 1;               // Start Line
+    temp = temp - 1;
+    if(direction) {
+        mother = temp;
+        son = 1;
+    } else {
+        mother = 1;
+        son = temp;
     }
-    std::cout << numerator << "/" << denominator;
+
+    for(int i = 0; i < (X - a); i++) {
+        if(direction) {
+            mother--;
+            son++;
+        } else {
+            mother++;
+            son--;
+        }
+    }
+
+    std::cout << son << "/" << mother;
+
 }
