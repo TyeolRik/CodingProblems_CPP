11a4df7f40e99764297fad31fbec5eab227970c2
TyeolRik <kino6147@naver.com>
2020-09-09 07:19:13 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1011.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/9) 수학 1/1011.cpp
@@ -1,7 +1,9 @@
 // https://www.acmicpc.net/problem/1011
 #include <iostream>
+#include <math.h>
 
 /*
+Pattern
      1: 1       1
      2: 2       1 1
      3: 3       1 1 1
@@ -18,31 +20,54 @@
     14: 7       1 2 3 3 2 2 1
     15: 7       1 2 3 3 3 2 1
     16: 7       1 2 3 4 3 2 1
-
-3 - 2
-4 - 2
-5 - 3
-6 - 3
-7 - 4
-8 - 4
-9 - 4
 */
 
-int length_to_hop(int length) {
-    
+long long test(long long length) {
+    double odd_min = sqrt(4 * length) - 1;
+    /*
+    double odd_max = 2 * sqrt(length);
+    double even_min = (-2 + sqrt(16 * length + 4)) / (double)2.0;
+    double even_max = sqrt(4 * length + 1);
+    std::cout << "홀: " << odd_min << " ~ " << odd_max << "\n";
+    std::cout << "짝: " << even_min << " ~ " << even_max << "\n";
+    */
+    return (long long)floor(odd_min);
+}
 
+long long length_to_hop(long long length) {
+
+    long long before_n = 3;
+    long long x = 3;
+    long long n = 3;
+
+    while(true) {
+        if(length == n) {
+            return x - 1;
+        } else if(length < n) {
+            return x - 1;
+        } else {
+            before_n = n;
+            if (x % 2 == 0) {
+                n = x / 2 * (2 + x) / 2;
+            } else {
+                n = ((x + 1) / 2) * (1 + x) / 2;
+            }
+            // std::cout << "before_n: " << before_n << "\tx: " << x << "\tn: " << n << "\n";
+            x++;
+        }
+    }
 }
 
 int main() {
-    int T, x, y;
+    long long T, x, y;
     std::cin >> T;
     while(T --> 0) {
         std::cin >> x >> y;
-        int length = y - x;
-        if(length < 3) {
-            std::cout << length;
+        long long length = y - x;
+        if(length <= 3) {
+            std::cout << length << "\n";
         } else {
-
+            std::cout << length_to_hop(length) << "\n";
         }
     }
 }
