80d24b811c4a4b4aee44d7d4bc6041c99e5abb8e
TyeolRik <kino6147@naver.com>
2020-09-10 04:17:38 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/1929.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/1929.cpp
@@ -1,1 +1,21 @@
 // https://www.acmicpc.net/problem/1929
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int M, N;
+    std::cin >> M >> N;
+    int sieve_of_Eratosthenes[1000000] = {0,};
+    for(int i = 2; i <= 1000; i++) {
+        int temp = 2;
+        while(temp * i < 1000000) {
+            sieve_of_Eratosthenes[temp++ * i] = 1;
+        }
+    }
+    sieve_of_Eratosthenes[1] = 1;       // 1은 소수가 아니다.
+    for(int i = M; i <= N; i++) {
+        if(sieve_of_Eratosthenes[i] == 0) {
+            std::cout << i << "\n";
+        }
+    }
+}
