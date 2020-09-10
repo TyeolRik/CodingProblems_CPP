6aac8d2b6799c59fd21ed55d08c8750f51cd72e6
TyeolRik <kino6147@naver.com>
2020-09-10 06:53:26 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/4948.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/4948.cpp
@@ -1,1 +1,29 @@
 // https://www.acmicpc.net/problem/4948
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int sieve[300000] = {0, };
+    for(int i = 2; i <= (int)sqrt(300000); i++) {
+        int temp = 2;
+        while(temp * i < 300000) {
+            sieve[temp++ * i] = 1;
+        }
+    }
+    sieve[1] = 1;           // 1은 소수가 아니다.
+
+    while(true) {
+        int n;
+        int count = 0;
+        std::cin >> n;
+        if(n == 0) {
+            break;
+        }
+        for(int i = n + 1; i <= 2*n; i++) {
+            if(sieve[i] == 0) {
+                count++;
+            }
+        }
+        std::cout << count << "\n";
+    }
+}
