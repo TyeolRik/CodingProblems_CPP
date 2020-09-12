deae7a643199c32ba199508d5192ad13908cbbec
TyeolRik <kino6147@naver.com>
2020-09-12 13:02:08 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/9020.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/9020.cpp
@@ -3,12 +3,15 @@
 
 int main() {
     int T, n;
+
+    // 에라토스테네스의 체 초기화
     int sieve_of_Eratosthenes[10000] = {0, };
     sieve_of_Eratosthenes[0] = 1;
     sieve_of_Eratosthenes[1] = 1;       // 1은 소수가 아니다.
     for(int i = 2; i < 100; i++) {
         int temp = 2;
         while(temp * i < 10000) {
+            // 0이면 소수, 1이면 소수가 아니다.
             sieve_of_Eratosthenes[(temp++) * i] = 1;
         }
     }
@@ -16,30 +19,16 @@ int main() {
     std::cin >> T;
     while(T --> 0) {
         std::cin >> n;
-        int small = 0;
-        int large = 0;
-        int difference = 10000;
-        for(int i = 3; i < n; i++) {
+        for(int i = (n / 2); i >= 2; i--) {
             if(sieve_of_Eratosthenes[i] != 0) {
+                // 소수가 아니면 넘어가기 :: 값이 0이어야 소수임
                 continue;
             }
-            for(int j = i; j < n; j++) {
-                if(sieve_of_Eratosthenes[j] != 0) {
-                    continue;
-                }
-                if(i + j == n) {
-                    if (difference > (j - i)) {
-                        small = i;
-                        large = j;
-                        difference = j - i;
-                    }
-                    if(difference == 0) {
-                        goto loop_breaker;
-                    }
-                }
+            // n = i + j 에서 정답이 되는 j = n - i 이므로,
+            if(sieve_of_Eratosthenes[n - i] == 0) {
+                std::cout << i << " " << n - i << "\n";
+                break;
             }
         }
-        loop_breaker:;
-        std::cout << small << " " << large << "\n";
     }
 }
