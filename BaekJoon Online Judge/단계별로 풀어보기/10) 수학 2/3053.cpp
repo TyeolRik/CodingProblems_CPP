9c619915d80b543c822db68889d1d0b08276a8cf
TyeolRik <kino6147@naver.com>
2020-09-13 17:21:29 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/3053.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/10) 수학 2/3053.cpp
@@ -1,1 +1,27 @@
 // https://www.acmicpc.net/problem/3053
+#include <iostream>
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+/*
+* Circle in Taxicab geometry
+
+- - * - -
+- * - * -
+* - O - *
+- * - * -
+- - * - -
+
+Look like above.
+*/
+
+
+int main() {
+    double R;
+    std::cin >> R;
+
+    std::cout.precision(10);
+    std::cout << std::fixed << R * R * M_PI << "\n";
+    std::cout << std::fixed << R * R * 2.0;
+}
