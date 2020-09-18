908b7d3bbecb44276e7e28bdf9717e7576e52fd3
TyeolRik <kino6147@naver.com>
2020-09-18 10:35:37 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/11) 재귀/11729.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/11) 재귀/11729.cpp
@@ -1,1 +1,32 @@
 // https://www.acmicpc.net/problem/11729
+#include <iostream>
+
+std::string str_temp;
+
+int hanoi(int number_of_disk, int from, int _and, int to) {
+    int temp = 0;
+    if(number_of_disk == 1) {
+        str_temp.append(std::to_string(from) + " " + std::to_string(to) + "\n");
+        //std::cout << from << " " << to << '\n';
+        temp = temp + 1;
+    } else {
+        temp = temp + hanoi(number_of_disk - 1, from, to, _and);
+        str_temp.append(std::to_string(from) + " " + std::to_string(to) + "\n");
+        // std::cout << from << " " << to << '\n';
+        temp = temp + 1;
+        temp = temp + hanoi(number_of_disk - 1, _and, from, to);
+    }
+    return temp;
+}
+
+int main() {
+    str_temp.reserve(10000000);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    int N;
+    std::cin >> N;
+
+    std::cout << hanoi(N, 1, 2, 3) << "\n";
+    std::cout << str_temp;
+}
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This project is for warming up before brain storming
 ## 기억해둘만한 문제번호
 - 1152
 - 1002 : float 형에서의 == 비교는 반드시 부등호로 비교해야함. ex) if(abs(a - b) < __DBL_EPSILON__)
+- 11729 : std::string의 operator**+** 는 느림. 그러므로, 미리 std::string something; something.reserve(10000000) 해두고 append해서 쓰는게 매우 빠름. ~~그냥 출력하는게 더 빠른거같다.~~
 
 ## Compile and Run
 - Press ```Ctrl + Shift + B``` in Visual Studio Code
