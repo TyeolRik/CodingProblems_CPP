bbf62d210cb0cca0986969b47a9f76ef46d8afba
TyeolRik <kino6147@naver.com>
2020-09-21 14:54:35 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/12) 브루트 포스/1436.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/12) 브루트 포스/1436.cpp
@@ -1,1 +1,20 @@
 // https://www.acmicpc.net/problem/1436
+#include <iostream>
+
+int main() {
+    int N;
+    int count = 600;
+    std::string input;
+    std::cin >> N;
+    while(N > 0) {
+        input = std::to_string(count++);
+        for(int i = 0; i < input.size() - 2; i++) {
+            if(input[i] == '6' && input[i+1] == '6' && input[i+2] == '6') {
+                N--;
+                goto personal_continue;
+            }
+        }
+        personal_continue:;
+    }
+    std::cout << input;
+}
