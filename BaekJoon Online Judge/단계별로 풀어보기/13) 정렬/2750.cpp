ff40a64c7e5f4123606275058b599e8487a0d1fc
TyeolRik <kino6147@naver.com>
2020-09-21 15:09:17 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/2750.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/2750.cpp
@@ -1,1 +1,16 @@
 // https://www.acmicpc.net/problem/2750
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int N;
+    std::cin >> N;
+    int* data = new int[N];
+    for(int i = 0; i < N; i++) {
+        std::cin >> data[i];
+    }
+    std::sort(data, data + N);
+    for(int i = 0; i < N; i++) {
+        std::cout << data[i] << "\n";
+    }
+}
--- BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/2751.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/2751.cpp
@@ -1,1 +1,21 @@
 // https://www.acmicpc.net/problem/2751
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int N;
+    std::cin >> N;
+    int* data = new int[N];
+    for(int i = 0; i < N; i++) {
+        std::cin >> data[i];
+    }
+
+    std::sort(data, data + N);
+    for(int i = 0; i < N; i++) {
+        std::cout << data[i] << "\n";
+    }
+}
