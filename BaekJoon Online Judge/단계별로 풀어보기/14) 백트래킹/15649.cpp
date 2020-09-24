11f2d1829d4b6892c67f02f4b15b33b96797045a
TyeolRik <kino6147@naver.com>
2020-09-24 17:05:23 +0000

    SOLVING

--- BaekJoon Online Judge/단계별로 풀어보기/14) 백트래킹/15649.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/14) 백트래킹/15649.cpp
@@ -1,1 +1,29 @@
 // https://www.acmicpc.net/problem/15649
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void comb(int N, int K)
+{
+    std::string bitmask(K, 1); // K leading 1's
+    bitmask.resize(N, 0); // N-K trailing 0's
+
+    // print integers and permute bitmask
+    do {
+        for (int i = 0; i < N; ++i) // [0..N-1] integers
+        {
+            if (bitmask[i]) std::cout << i + 1 << " ";
+        }
+        std::cout << std::endl;
+    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    int N, M;
+    std::cin >> N >> M;
+
+    comb(N, M);
+}
