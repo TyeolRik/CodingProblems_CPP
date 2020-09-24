cdcfec5c8cfb26e0e8e83a521625edfec396e9b8
TyeolRik <kino6147@naver.com>
2020-09-24 11:49:25 +0000

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/1427.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/13) 정렬/1427.cpp
@@ -1,1 +1,14 @@
 // https://www.acmicpc.net/problem/1427
+#include <iostream>
+#include <algorithm>
+
+bool desc(char a, char b) {
+    return a > b;
+}
+
+int main() {
+    std::string N;
+    std::cin >> N;
+    std::sort(N.begin(), N.end(), desc);
+    std::cout << N;
+}
