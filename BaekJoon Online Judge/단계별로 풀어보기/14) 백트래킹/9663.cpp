b7a6022d0c6068ce4d0ddc8a0db4c08ff768fde3
TyeolRik <kino6147@naver.com>
2021-02-12 02:59:30 +0900

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/14) 백트래킹/9663.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/14) 백트래킹/9663.cpp
@@ -1,123 +1,80 @@
-// https://www.acmicpc.net/problem/9663
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <numeric>
-
-/*
-class Block {
-    public:
-    unsigned int positionX;
-    unsigned int positionY;
-    bool isQueen;
-
-    Block() {
-
-    };
-
-    Block(unsigned int posX, unsigned int posY)
-        : positionX(posX), positionY(posY), isQueen(false) {};
-
-    void getBlockInfo() {
-        // std::cout << isQueen << "\n";
-        if(isQueen) {
-            std::cout << "(" << positionX << ", " << positionY << ") QUEEN\n";
-        } else {
-            std::cout << "(" << positionX << ", " << positionY << ") BLANK\n";
-        }
-    }
-
-};
-*/
+#include <algorithm>
 
 typedef struct Block {
-    int positionX, positionY;
+    int row, col;
 } Block;
 
 std::vector<Block> stack;
-std::vector<std::vector<int>> aliveCOLUMN;
-int answerCount = 0;
-int boardSize;
+int BoardSize;
+int solutionCount = 0;
 
-/*
-void printChessBoard(std::vector<std::vector<Block>> board) {
-    for(std::vector<Block> each1 : board) {
-        for(Block each2 : each1) {
-            each2.getBlockInfo();
-        }
-        std::cout << "\n";
+std::vector<int> getPossibleChildren(Block current) {
+    std::vector<int> ret(BoardSize);
+    std::iota(ret.begin(), ret.end(), 0);
+    if(current.col == 0) {
+        ret.erase(std::remove(ret.begin(), ret.end(), 0), ret.end());
+        ret.erase(std::remove(ret.begin(), ret.end(), 1), ret.end());
+    } else if(current.col == (BoardSize - 1)) {
+        ret.erase(std::remove(ret.begin(), ret.end(), (BoardSize - 1)), ret.end());
+        ret.erase(std::remove(ret.begin(), ret.end(), (BoardSize - 2)), ret.end());
+    } else {
+        ret.erase(std::remove(ret.begin(), ret.end(), (current.col - 1)), ret.end());
+        ret.erase(std::remove(ret.begin(), ret.end(), current.col), ret.end());
+        ret.erase(std::remove(ret.begin(), ret.end(), (current.col + 1)), ret.end());
     }
-}
-*/
+    for(Block each : stack) {
+        ret.erase(std::remove(ret.begin(), ret.end(), (each.col)), ret.end());
+    }
 
-bool isPossiblePutQueen(Block A, Block B) {
-    int aX = A.positionX;
-    int bX = B.positionX;
-    int aY = A.positionY;
-    int bY = B.positionY;
-    int tempX = aX - bX;
-    int tempY = aY - bY;
-    // 옆에 있으면 불가능 || 세로가 같아도 불가능 || 대각선이면 불가능
-    if((aX == bX) || (aY == bY) || (tempX == tempY) || (tempX == tempY * -1)) {
-        return false;
-    }
-    return true;
+    return ret;
 }
 
-bool isAllOK(std::vector<Block> _stack, Block _attempt_to_input) {
-    for(Block each : _stack) {
-        if(!isPossiblePutQueen(each, _attempt_to_input)) {
+bool isAllOK(Block testBlock) {
+    for(Block each : stack) {
+        // 대각선으로 일치하는지만 체크하면 됨
+        if((each.row - testBlock.row) == (each.col - testBlock.col) || (each.row - testBlock.row) == (each.col - testBlock.col) * -1) {
             return false;
         }
     }
     return true;
 }
 
-void DFS(std::vector<std::vector<Block>> graph, int recursive_count) {
-
-    // 재귀함수의 탈출조건
-    if(recursive_count == 0) {
-        answerCount++;
+void DFS(Block currentBlock) {
+    // 탈출조건
+    if(currentBlock.row == (BoardSize - 1)) {
+        solutionCount++;
         return;
     }
 
-    for(int i = 0; i < boardSize; i++) { 
-        Block test;
-        if(stack.size() == 0) {
-            test = {0, i};
-        } else {
-            test = {stack.back().positionX + 1, i};
-        }
-        if(!isAllOK(stack, test)) {
+    for(int each : getPossibleChildren(currentBlock)) {
+        Block testBlock = {currentBlock.row + 1, each};
+        if(!isAllOK(testBlock)) {
             continue;
+        } else {
+            stack.push_back(testBlock);
+            DFS(testBlock);
+            stack.pop_back();
         }
-        stack.push_back(test);
-        DFS(graph, recursive_count - 1);
-        stack.pop_back();
-
     }
-    
 }
 
+void DFS() {
+    for(int i = 0; i < BoardSize; i++) {
+        Block firstBlock = {0, i};
+        stack.push_back(firstBlock);
+        DFS(firstBlock);
+        stack.pop_back();
+    }
+}
 
 int main() {
     int N;
     std::cin >> N;
-    std::vector<std::vector<Block>> chessBoard;
-    boardSize = N;
+    BoardSize = N;
 
-    chessBoard.reserve(N);
-
-    for(int i = 0; i < N; i++) {
-        std::vector<Block> eachLine;
-        eachLine.reserve(N);
-        for(int j = 0; j < N; j++) {
-            eachLine.push_back({i, j});
-        }
-        chessBoard.push_back(eachLine);
-    }
-    
-    DFS(chessBoard, N);
-
-    std::cout << answerCount << "\n";
+    DFS();
+    std::cout << solutionCount << "\n";
 }
