00146e6a6aead0a436d81596a58c4c7446872f20
TyeolRik <kino6147@naver.com>
2021-02-23 02:14:46 +0900

    SOLVED

--- BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/1003.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/1003.cpp
@@ -1,1 +1,30 @@
 // https://www.acmicpc.net/problem/1003
+#include <iostream>
+#include <vector>
+
+std::vector<int> fibo(41);
+
+int main() {
+    std::cin.tie(NULL);
+    std::cin.sync_with_stdio(false);
+
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int i = 2; i < 41; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+
+    int T, N;
+    std::cin >> T;
+
+    for(int i = 0; i < T; i++) {
+        std::cin >> N;
+        if(N == 0) {
+            std::cout << 1 << ' ' << 0 << '\n';
+        } else if(N == 1) {
+            std::cout << 0 << ' ' << 1 << '\n';
+        } else {
+            std::cout << fibo[N-1] << ' ' << fibo[N] << '\n';
+        }
+    }
+}
--- BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/9184.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/9184.cpp
@@ -3,32 +3,60 @@
 #include <vector>
 #include <numeric>
 
-short w[21][21][21];
+#define UNDEFINED   -9999
+
+int w[21][21][21];
+
+int calc(int a, int b, int c) {
+    if(a <= 0 || b <= 0 || c <= 0) {
+        return 1;
+    } else if(a > 20 || b > 20 || c > 20) {
+        return w[20][20][20];
+    } else if(w[a][b][c] != UNDEFINED) {
+        return w[a][b][c];
+    } else if(a < b && b < c) {
+        int ret = calc(a, b, c-1) + calc(a, b-1, c-1) - calc(a, b-1, c);
+        w[a][b][c] = ret;
+        return ret;
+    } else {
+        int ret = calc(a-1, b, c) + calc(a-1, b-1, c) + calc(a-1, b, c-1) - calc(a-1, b-1, c-1);
+        w[a][b][c] = ret;
+        // std::cout << "w(" << a << ", " << b << ", " << c << ") = " << w[a][b][c] << "\n";
+        return ret;
+    }
+}
 
 void initialize() {
-    w[0][0][0] = 1;
-    w[0][0][1] = 1;
-    w[0][1][0] = 1;
-    w[0][1][1] = 1;
-    w[1][0][0] = 1;
-    w[1][0][1] = 1;
-    w[1][1][0] = 1;
-    
-    
+    for(int i = 0; i < 21; i++) {
+        for(int j = 0; j < 21; j++) {
+            for(int k = 0; k < 21; k++) {
+                w[i][j][k] = UNDEFINED;
+            }
+        }
+    }
+    for(int i = 0; i < 21; i++) {
+        for(int j = 0; j < 21; j++) {
+            for(int k = 0; k < 21; k++) {
+                w[i][j][k] = calc(i, j, k);
+            }
+        }
+    }
 }
 
 int main() {
 
     // Initialize Data
     initialize();
-    short a = 0;
-    short b = 0;
-    short c = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
 
     while(true) {
         std::cin >> a >> b >> c;
-        if(a == -1 && b == -1 && c == -1) {
-            exit(-1);
+        if((a == -1) && (b == -1) && (c == -1)) {
+            exit(0);
+        } else if(a <= 0 || b <= 0 || c <= 0) {
+            std::cout << "w(" << a << ", " << b << ", " << c << ") = " << 1 << "\n";
         } else if(a > 20 || b > 20 || c > 20) {
             std::cout << "w(" << a << ", " << b << ", " << c << ") = " << w[20][20][20] << "\n";
         } else {
