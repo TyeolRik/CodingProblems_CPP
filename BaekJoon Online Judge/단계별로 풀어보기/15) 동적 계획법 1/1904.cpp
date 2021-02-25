b13aaed23a7a522201a9f3460da0545fb37dcf0a
TyeolRik <kino6147@naver.com>
2021-02-25 23:04:25 +0900

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/1904.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/1904.cpp
@@ -1,1 +1,40 @@
 // https://www.acmicpc.net/problem/1904
+#include <iostream>
+#include <vector>
+
+unsigned long long calc(int input) {
+    unsigned long long a = 1;
+    unsigned long long b = 2;
+    unsigned long long ret;
+
+    for(int i = 2; i < input; i++) {
+        ret = (a + b) % 15746;
+        a = b;
+        b = ret;
+    }
+    return ret;
+}
+
+int main() {
+    int N;
+    std::cin >> N;
+    
+    switch (N)
+    {
+    case 1:
+        std::cout << 1;
+        break;
+    
+    case 2:
+        std::cout << 2;
+        break;
+    
+    case 3:
+        std::cout << 3;
+        break;
+    
+    default:
+        std::cout << calc(N) % 15746;
+        break;
+    }
+}
