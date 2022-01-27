2680f5c5f09ec75b06960cff6c18591efeafdd5b
TyeolRik <kino6147@naver.com>
2022-01-27 18:29:59 +0900

    SOLVE

--- BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/9251.cpp
+++ BaekJoon Online Judge/단계별로 풀어보기/15) 동적 계획법 1/9251.cpp
@@ -4,15 +4,28 @@
 #include <algorithm>
 
 int main() {
-    std::string string1;
-    std::string string2;
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
 
-    std::cin >> string1;
-    std::cin >> string2;
+    s1.insert(s1.begin(), '0');
+    s2.insert(s2.begin(), '0');
 
-    std::vector<int> save(string2.size());
-
-    for(int i = 0; i < string2.size(); i++) {
-        
+    std::vector<std::vector<int>> dp(s2.size());
+    for(int row = 0; row < dp.size(); row++) {
+        dp[row] = std::vector<int>(s1.size());
+        for(int col = 0; col < dp[row].size(); col++) {
+            if(row == 0 || col == 0) {
+                dp[row][col] = 0;
+                continue;
+            }
+            if(s2[row] == s1[col]) {
+                dp[row][col] = dp[row-1][col-1] + 1;
+                continue;
+            } else {
+                dp[row][col] = std::max(dp[row-1][col], dp[row][col-1]);
+            }
+        }
     }
+    std::cout << dp[s2.size()-1][s1.size()-1];
+    return 0;
 }
